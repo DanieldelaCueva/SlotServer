922c7db51ba9cfd8fb8b9dbf12e621c744fd78c8
DanieldelaCueva <danielcuevapuertas@gmail.com>
2022-08-25 11:05:51 +0000

    deployed to pythonanywhere

--- slotserver/slotserver/settings.py
+++ slotserver/slotserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-1982s(%8)ljc_*vsn8aiba&h33(siy7uv7f1wan+e5wv=%qu03
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'danieldelacueva.pythonanywhere.com'
+    ]
 
 
 # Application definition
@@ -123,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
